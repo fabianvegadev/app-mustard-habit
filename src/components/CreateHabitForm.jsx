63dda9ea7c9692fe0fe4cfa442102b798e0a1b1c
Fabian Velasquez <fabianvegadev@gmail.com>
2024-09-18 19:42:10 -0500

    Agregando componetes nuevos para actualizar y delegando archivo NavBar a Leonardo

---
+++ src/components/CreateHabitForm.jsx
@@ -0,0 +1,20 @@
+function CreateHabitForm () {
+
+    return (
+            <form>
+                <textarea>
+
+                </textarea>
+                <div>
+                    <button>
+
+                    </button>
+                    <button>
+
+                    </button>
+                </div>
+            </form>
+    )
+}
+
+export {CreateHabitForm};
