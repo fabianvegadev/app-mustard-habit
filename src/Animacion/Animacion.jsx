832d752b11df223291e1e8e2462bdfa429e92763
Fabian Velasquez <fabianvegadev@gmail.com>
2024-09-22 00:11:56 -0500

    Agregando animacion de fondo

---
+++ src/Animacion/Animacion.jsx
@@ -0,0 +1,15 @@
+import './Animacion.css';
+
+function Animacion () {
+
+    
+    
+    return (        
+        <div className="circle-container">
+            <div className="circle">
+            </div>
+        </div>       
+    )
+}
+
+export { Animacion };
